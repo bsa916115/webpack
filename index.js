1a6d755ca781b96650942345ab822e2b347b993d
Borisov Sergey <bsa916115@mail.ru>
2021-01-16 02:59:39 +0300

    Correct webpack.config

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import Counter from './counter.js';
 
-cnt = new Counter(10);
+let cnt = new Counter(10);
 
-console.log('Counter initial = ' + cnt.getCounter)
+console.log('Counter initial = ' + cnt.getCounter())
 
+cnt.increment()
+cnt.increment()
 
+console.log(`Counter after 2 increments = ${cnt.getCounter()}`)
+
+cnt.decrement()
+
+console.log(`Counter after 1 decrement = ${cnt.getCounter()}`)
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,20 @@ module.exports =
   output:
   {
     filename: 'build.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
