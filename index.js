748f6d64b16dc1bf8ae02ae266ab8d99e4d61690
Borisov Sergey <bsa916115@mail.ru>
2021-01-15 20:10:17 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import Counter from './counter.js';
+
+cnt = new Counter(10);
+
+console.log('Counter initial = ' + cnt.getCounter)
+
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+  entry: './index.js',
+  output:
+  {
+    fiilename: 'build.js',
+    output: './build'
+  }
+}
